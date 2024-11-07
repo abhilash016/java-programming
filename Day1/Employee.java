fbbd8d6233b6dba855e6d3dafcc2b9eb1a93cea3
abhilash016 <abhinandha796@gmail.com>
2024-11-07 20:20:36 +0530

    Add files via upload

---
+++ Day1/Employee.java
@@ -0,0 +1,10 @@
+class Employee{
+ public static void main(String args[]){
+  int a=150;
+  String name="abhilash";
+  float ft=555.65f;
+  System.out.println("The given employee id is :"+a);
+  System.out.println("The given employee name is :"+name);
+  System.out.println("The given employee salary is :"+ft);
+}
+}
---
+++ Day1/Stu.java
@@ -0,0 +1,13 @@
+class Stu
+{
+  public static void main(String args[])
+{
+  int rollno = 16;
+  String name = "Abhilash";
+  float cgpa = 8.5f;
+  System.out.println("The given student rollno is:" +rollno);
+  System.out.println("The given student name is:" +name);
+  System.out.println("The given student cgpa is:" +cgpa);
+}
+}
+  
